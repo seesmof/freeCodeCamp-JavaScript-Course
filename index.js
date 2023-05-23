d2f12e345433cf307c5b03f50fee9be01e0bc4e1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-23 16:34:46 +0300

    feat: assigning values to variables

--- index.js
+++ index.js
@@ -13,13 +13,20 @@ var inputBox = document.getElementById("variables-alert-input-box");
 var submitButton = document.getElementById("variables-alert-submit-button");
 submitButton.addEventListener("click", function () {
   let inputBoxValue = inputBox.value;
-
   if (inputBoxValue === "") {
     alert("Whoops... You didn't enter anything");
     return;
   }
-
   let outputText = "You just said:  " + inputBoxValue;
   alert(outputText);
   inputBox.value = "";
 });
+
+var myName;
+// calculations are performed before the variable is assigned with the value
+
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
