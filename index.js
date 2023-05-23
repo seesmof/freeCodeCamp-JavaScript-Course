ae86262c5d200517d985f10dbddf6845c59e6bec
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-23 18:53:25 +0300

    feat: Subtract One Number from Another with JavaScript

--- index.js
+++ index.js
@@ -258,3 +258,7 @@ constsParagraph.innerHTML = FCC + " " + fact;
 // # Add Two Numbers with JavaScript
 
 const sum = 10 + 10;
+
+// # Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
