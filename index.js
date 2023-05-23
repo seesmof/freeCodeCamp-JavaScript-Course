24d24b75c201442cbbd6571291123df1bc674d3d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-23 16:30:00 +0300

    feat: add event listener onto an input box and output content of it as an alert

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ let sectionHeadingText = "# " + sectionHeading.textContent;
 sectionHeading.innerHTML = sectionHeadingText;
 
 var inputBox = document.getElementById("variables-alert-input-box");
+var submitButton = document.getElementById("variables-alert-submit-button");
+submitButton.addEventListener("click", function () {
+  let inputBoxValue = inputBox.value;
+
+  if (inputBoxValue === "") {
+    alert("Whoops... You didn't enter anything");
+    return;
+  }
+
+  let outputText = "You just said:  " + inputBoxValue;
+  alert(outputText);
+  inputBox.value = "";
+});
