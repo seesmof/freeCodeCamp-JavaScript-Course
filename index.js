76a7625020bfd29f2c602c97e08ad6faf76d1e3b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-23 18:43:56 +0300

    feat: Understanding Case Sensitivity in Variables

--- index.js
+++ index.js
@@ -215,3 +215,13 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+// # Understanding Case Sensitivity in Variables
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
