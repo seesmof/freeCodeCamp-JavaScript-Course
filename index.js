7031f89e2446e2c428836ddec89c70e8eba1ae58
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-23 16:25:54 +0300

    feat: variables

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import "/index.css";
 
+// COMMENTS
 // JavaScript is awesome!
+/* JavaScript is awesome! */
+
+// VARIABLES
+let sectionHeading = document.querySelector(".section-heading");
+let sectionHeadingText = "# " + sectionHeading.textContent;
+sectionHeading.innerHTML = sectionHeadingText;
+
+var inputBox = document.getElementById("variables-alert-input-box");
