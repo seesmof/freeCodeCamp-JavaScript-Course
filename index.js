7a59eb1ee2d40b104db329bce24cc60ccf1070cb
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-25 22:29:39 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -498,3 +498,16 @@ cannot override these now
     console.log(output);
   }
 }
+{
+  function myLocalScope() {
+    // Only change code below this line
+    let myVar;
+
+    console.log("inside myLocalScope", myVar);
+  }
+  myLocalScope();
+
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log("outside myLocalScope", myVar);
+}
