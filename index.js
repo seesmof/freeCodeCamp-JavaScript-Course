b1e4cfd1bab165c80b0c2272bd9aa0d1c45506f2
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-25 20:31:05 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -385,3 +385,13 @@ cannot override these now
   // Only change code below this line
   myArray[0] = 45;
 }
+{
+  const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+
+  const myData = myArray[2][1];
+}
