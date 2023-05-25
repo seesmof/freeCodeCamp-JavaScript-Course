00bf024c954ceeafd6350071008b474de29f8ec4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-25 20:33:25 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -405,3 +405,13 @@ cannot override these now
   // Only change code below this line
   myArray.push(["dog", 3]);
 }
+{
+  // Setup
+  const myArray = [
+    ["John", 23],
+    ["cat", 2],
+  ];
+
+  // Only change code below this line
+  const removedFromMyArray = myArray.pop();
+}
