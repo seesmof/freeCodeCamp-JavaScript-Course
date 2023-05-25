ddd51b9fa6215da339d3d4b1612d1adfc10bac75
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-25 22:31:32 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -525,3 +525,21 @@ cannot override these now
 
   myOutfit();
 }
+{
+  // Setup
+  let sum = 0;
+
+  function addThree() {
+    sum = sum + 3;
+  }
+
+  // Only change code below this line
+  function addFive() {
+    sum = sum + 5;
+  }
+
+  // Only change code above this line
+
+  addThree();
+  addFive();
+}
