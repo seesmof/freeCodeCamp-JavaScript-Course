45c225a62b7583229566d0580a8512daa9e9f90a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-25 20:20:39 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -344,3 +344,10 @@ cannot override these now
   // Only change code below this line
   const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 }
+{
+  // Setup
+  const lastName = "Lovelace";
+
+  // Only change code below this line
+  const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+}
