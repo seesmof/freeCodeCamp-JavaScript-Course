64f1c358caf258cd443252a5833666e26578a3a1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 22:44:58 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -627,3 +627,27 @@ cannot override these now
 
   compareEquality(10, "10");
 }
+{
+  // Setup
+  function testNotEqual(val) {
+    if (val != 99) {
+      // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+
+  testNotEqual(10);
+}
+{
+  // Setup
+  function testStrictNotEqual(val) {
+    if (val !== 17) {
+      // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+
+  testStrictNotEqual(10);
+}
