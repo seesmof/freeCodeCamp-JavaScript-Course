0fe145a3c0fc7c259b66c36fe040d9f4c5f811af
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 22:42:52 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -615,3 +615,15 @@ cannot override these now
 
   testStrict(10);
 }
+{
+  // Setup
+  function compareEquality(a, b) {
+    if (a === b) {
+      // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+
+  compareEquality(10, "10");
+}
