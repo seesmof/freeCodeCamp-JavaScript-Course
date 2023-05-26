49ecff7a6a5de9944748afefe1c715e340abd41a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 23:11:29 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -956,3 +956,12 @@ cannot override these now
 
   chainToSwitch(7);
 }
+{
+  function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+
+  isLess(10, 15);
+}
