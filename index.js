5500d0b930d02b7c30fd0b1128fdf00425244e5b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 22:53:14 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -764,3 +764,16 @@ cannot override these now
 
   testElse(4);
 }
+{
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else {
+      return "Between 5 and 10";
+    }
+  }
+
+  testElseIf(7);
+}
