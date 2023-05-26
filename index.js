11103b76468237f89750bb6061d4844a09b93ef4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 22:36:41 +0300

    modified index.js in .

--- index.js
+++ index.js
@@ -581,3 +581,13 @@ cannot override these now
     // Only change code above this line
   }
 }
+{
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+    // Only change code above this line
+  }
+}
