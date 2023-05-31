7abe8667d575919140c590c5ccbad1e8bcd607c9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 23:05:54 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1013,3 +1013,14 @@ cannot override these now
 
   updateRecords(recordCollection, 5439, "artist", "ABBA");
 }
+{
+  // Setup
+  const myArray = [];
+
+  // Only change code below this line
+  let i = 5;
+  while (i >= 0) {
+    myArray.push(i);
+    i--;
+  }
+}
