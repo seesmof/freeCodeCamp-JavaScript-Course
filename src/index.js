534b2db6ffb5306009772f694c8690f6aff9377e
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 23:10:21 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1047,3 +1047,13 @@ cannot override these now
   // Only change code below this line
   for (let i = 9; i >= 1; i -= 2) myArray.push(i);
 }
+{
+  // Setup
+  const myArr = [2, 3, 4, 5, 6];
+
+  // Only change code below this line
+  let total = 0;
+  for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+  }
+}
