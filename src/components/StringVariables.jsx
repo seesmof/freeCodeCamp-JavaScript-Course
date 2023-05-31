0e4b480337b60294159403e651543e5e4a459dd1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 18:50:20 +0300

    modified StringVariables.jsx and index.js

--- src/components/StringVariables.jsx
+++ src/components/StringVariables.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const StringVariables = () => {
+  const firstNameInput = useRef(null);
+  const lastNameInput = useRef(null);
+
+  const submitButton = () => {
+    const firstName = firstNameInput.current.value;
+    const lastName = lastNameInput.current.value;
+    alert(`Hello, ${firstName} ${lastName}\nHow are you doin?`);
+  };
+
   return (
     <section class="section-template">
       <h2 class="section-heading">string variables</h2>
@@ -11,15 +20,21 @@ const StringVariables = () => {
           id="string-first-name-input"
           class="input-field-underline"
           placeholder="Enter first name..."
+          ref={firstNameInput}
         />
         <input
           type="text"
           id="string-last-name-input"
           class="input-field-underline"
           placeholder="Enter last name..."
+          ref={lastNameInput}
         />
       </div>
-      <button class="button-gray button mt-3" id="string-submit-button">
+      <button
+        class="button-gray button mt-3"
+        id="string-submit-button"
+        onClick={submitButton}
+      >
         Submit
       </button>
     </section>
