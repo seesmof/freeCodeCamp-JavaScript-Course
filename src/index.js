950e7c3fc85f6d4a051ad5a9fc8fb69b0361843d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 22:57:50 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -958,3 +958,17 @@ cannot override these now
 
   const gloveBoxContents = myStorage.car.inside["glove box"];
 }
+{
+  const myPlants = [
+    {
+      type: "flowers",
+      list: ["rose", "tulip", "dandelion"],
+    },
+    {
+      type: "trees",
+      list: ["fir", "pine", "birch"],
+    },
+  ];
+
+  const secondTree = myPlants[1].list[1];
+}
