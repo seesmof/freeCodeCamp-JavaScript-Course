5e24ca473a52757aab7df0bfff1218713b80d4b0
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 16:11:19 +0000

    🌿: Wednesday, May 31, 2023 at 7:11:19 PM Eastern European Summer Time

--- src/components/Arrays.jsx
+++ src/components/Arrays.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Arrays = () => {
+  const myList = [
+    ["lettuce", 2],
+    ["cheese", 3],
+    ["carrots", 4],
+    ["orange", 5],
+    ["pizza", 6],
+    ["mushrooms", 7],
+  ];
+
   return (
     <section class="section-template">
       <h2 class="section-heading">Arrays</h2>
 
-      <ul class="list" id="array-list">
-        <p class="pb-2 font-medium">Here is my shopping cart list:</p>
-      </ul>
+      <ul class="list" id="array-list"></ul>
       <p class="pt-2 font-medium">
         The total price is
         <span
