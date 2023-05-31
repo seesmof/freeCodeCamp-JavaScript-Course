27da0944fd94ad9b2b09df5d148f7aa464251ce1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 21:53:10 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -782,3 +782,34 @@ cannot override these now
 
   abTest(2, 2);
 }
+{
+  let count = 0;
+
+  function cc(card) {
+    // Only change code below this line
+    if (card == 2 || card == 3 || card == 4 || card == 5 || card == 6) {
+      count++;
+    } else if (
+      card == 10 ||
+      card == "J" ||
+      card == "Q" ||
+      card == "K" ||
+      card == "A"
+    ) {
+      count--;
+    }
+
+    if (count <= 0) {
+      return count + " Hold";
+    } else {
+      return count + " Bet";
+    }
+    // Only change code above this line
+  }
+
+  cc(2);
+  cc(3);
+  cc(7);
+  cc("K");
+  cc("A");
+}
