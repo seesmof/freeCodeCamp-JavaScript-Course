eefdf59715691bae88540a6735a942dbb22dd880
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 22:07:08 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -849,3 +849,27 @@ cannot override these now
   const entreeValue = testObj["an entree"]; // Change this line
   const drinkValue = testObj["the drink"]; // Change this line
 }
+{
+  // Setup
+  const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas",
+  };
+
+  // Only change code below this line
+  const playerNumber = 16; // Change this line
+  const player = testObj[playerNumber]; // Change this line
+}
+{
+  // Setup
+  const myDog = {
+    name: "Coder",
+    legs: 4,
+    tails: 1,
+    friends: ["freeCodeCamp Campers"],
+  };
+
+  // Only change code below this line
+  myDog["name"] = "Happy Coder";
+}
