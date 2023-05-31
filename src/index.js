21e7fdd423893e67f36dbc1c434c41b1cdb995ba
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-31 22:13:38 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -897,3 +897,25 @@ cannot override these now
   // Only change code below this line
   delete myDog.tails;
 }
+{
+  // Setup
+  function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    const lookup = {
+      alpha: "Adams",
+      bravo: "Boston",
+      charlie: "Chicago",
+      delta: "Denver",
+      echo: "Easy",
+      foxtrot: "Frank",
+    };
+    result += lookup[val];
+
+    // Only change code above this line
+    return result;
+  }
+
+  phoneticLookup("charlie");
+}
