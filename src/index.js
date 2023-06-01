9cf56ad52691760cd4a37b4346f4d5ddf677de67
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-01 23:47:00 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1098,3 +1098,49 @@ cannot override these now
     // Only change code above this line
   }
 }
+{
+  // Setup
+  const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let i = 0; i < contacts.length; i++) {
+      for (let j = 0; j < contacts[i].length; j++) {
+        if (
+          contacts[i].firstName === name &&
+          contacts[i].hasOwnProperty(prop)
+        ) {
+          return contacts[i][prop];
+        }
+      }
+    }
+    // Only change code above this line
+  }
+
+  lookUpProfile("Akira", "likes");
+}
