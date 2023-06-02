1e522ec8e9b2ffe3b0556f02323f2028176aa58e
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 18:18:10 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1264,3 +1264,8 @@ cannot override these now
 
   console.log(myConcat([1, 2], [3, 4, 5]));
 }
+{
+  // Only change code below this line
+  const increment = (number, value = 1) => number + value;
+  // Only change code above this line
+}
