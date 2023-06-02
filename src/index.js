3197673ed6c1fd40e5c71b6c90fd5add8b02af0c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 08:28:19 +0000

    🌿: Friday, June 2, 2023 at 11:28:19 AM Eastern European Summer Time

--- src/index.js
+++ src/index.js
@@ -1188,3 +1188,16 @@ cannot override these now
 
   checkSign(10);
 }
+{
+  // Only change code below this line
+  function countdown(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1);
+      arr.unshift(n);
+      return arr;
+    }
+  }
+  // Only change code above this line
+}
