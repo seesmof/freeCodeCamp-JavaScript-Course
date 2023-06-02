5ca5cb577a40bd82e4a72aeb0d45809aa1d4fc5a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 19:20:43 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1312,3 +1312,18 @@ cannot override these now
 
   // Only change code above this line
 }
+{
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 },
+  };
+
+  // Only change code below this line
+
+  const {
+    today: { low: lowToday, high: highToday },
+  } = LOCAL_FORECAST;
+
+  // Only change code above this line
+}
