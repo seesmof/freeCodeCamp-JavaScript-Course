49ab4bb35507de0e98d2b4c3120475bb1eaf6a0f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 18:10:56 +0000

    🌿: Friday, June 2, 2023 at 9:10:56 PM Eastern European Summer Time

--- src/components/Arrays.jsx
+++ src/components/Arrays.jsx
@@ -9,21 +9,60 @@ const Arrays = () => {
     ["pizza", 6],
     ["mushrooms", 7],
   ];
+  const randomlyGeneratedArray = [];
+  for (let i = 0; i < 10; i++) {
+    randomlyGeneratedArray.push(Math.floor(Math.random() * 11));
+  }
 
   return (
     <section class="section-template">
       <h2 class="section-heading">Arrays</h2>
 
-      <p className="mb-2 font-medium">My shopping list ({myList.length}):</p>
-      <p className="">
-        {myList.map((item) => {
-          return (
-            <div>
-              {item[0]} - {item[1]}
+      <div className="flex flex-col gap-4">
+        <article className="flex flex-col">
+          <p className="mb-2 font-medium">
+            My shopping list ({myList.length}):
+          </p>
+          <p className="">
+            {myList.map((item) => {
+              return (
+                <div>
+                  {item[0]} - {item[1]}
+                </div>
+              );
+            })}
+          </p>
+        </article>
+
+        <article className="flex flex-col">
+          <p className="mb-2 font-medium">Array Methods</p>
+
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-row items-end gap-4">
+              <input
+                type="text"
+                className="input-field-underline"
+                placeholder="Number of elements..."
+              />
+              <button className="button button-gray">Generate Array</button>
             </div>
-          );
-        })}
-      </p>
+
+            <div className="flex flex-col">
+              <p className="font-medium">
+                Randomly Generated Array:{" "}
+                <span className="font-normal">{randomlyGeneratedArray}</span>
+                <div className="flex flex-row flex-wrap gap-1">
+                  <button className="button button-green">Sort</button>
+                  <button className="button button-yellow">Reverse</button>
+                  <button className="button button-red">Generate</button>
+                  <button className="button button-blue">Copy</button>
+                  <button className="button button-gray">Clear</button>
+                </div>
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
