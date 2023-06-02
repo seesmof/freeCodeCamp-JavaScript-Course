b52b48c5a01ca9ef8298691e2f2a3ba15e9fc6c8
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 18:25:28 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1278,3 +1278,11 @@ cannot override these now
     return total;
   };
 }
+{
+  const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+  let arr2;
+
+  arr2 = [...arr1]; // Change this line
+
+  console.log(arr2);
+}
