bb843c8905f1b1f770f3a7c1ec48ae1472bfc584
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 19:18:22 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1299,3 +1299,16 @@ cannot override these now
 
   // Only change code above this line
 }
+{
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80,
+  };
+
+  // Only change code below this line
+
+  const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+  // Only change code above this line
+}
