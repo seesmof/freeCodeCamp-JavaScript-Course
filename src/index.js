0d30f40da4ebbd52529d9bcab3792c57f3a6427a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-02 19:49:20 +0000

    🌿: Friday, June 2, 2023 at 10:49:20 PM Eastern European Summer Time

--- src/index.js
+++ src/index.js
@@ -1358,3 +1358,19 @@ cannot override these now
   const half = ({ max, min }) => (max + min) / 2.0;
   // Only change code above this line
 }
+{
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"],
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    // Only change code above this line
+
+    return failureItems;
+  }
+
+  const failuresList = makeList(result.failure);
+}
