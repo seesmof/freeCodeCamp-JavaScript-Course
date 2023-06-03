46ab9b07c00af1b95c69f7c132f139e841a0b43f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-03 15:09:20 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1387,3 +1387,15 @@ cannot override these now
 
   var createPerson = (name, age, gender) => ({ name, age, gender });
 }
+{
+  // Only change code below this line
+  const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    },
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+}
