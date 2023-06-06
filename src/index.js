d9c202526d5cacc8477157e1b655a488f2415f23
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-06 14:28:27 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1401,7 +1401,11 @@ cannot override these now
 }
 {
   // Only change code below this line
-
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
   // Only change code above this line
 
   const carrot = new Vegetable("carrot");
