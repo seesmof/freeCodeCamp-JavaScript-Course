c6f6e5632fc382604a02e06f6d45f107a7787fc3
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-06 15:17:37 +0300

    modified Classes.jsx in components

--- src/components/Classes.jsx
+++ src/components/Classes.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { BsPencil } from "react-icons/bs";
-import { RxCross1 } from "react-icons/rx";
+import { RxCross1, RxPencil1 } from "react-icons/rx";
+import { BiCake } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Classes = () => {
   class User {
@@ -11,14 +12,30 @@ const Classes = () => {
     }
   }
 
-  const users = [
+  const [users, setUsers] = useState([
     new User("Jane Doe", 30, "jane@doe.com"),
     new User("John Doe", 25, "john@doe.com"),
-  ];
+  ]);
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
+  };
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+  };
+
+  const removeUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const editUser = (name, age, email) => {
+    const editedUser = new User(name, age, email);
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers([...filteredUsers, editedUser]);
+    setSearchQuery("");
   };
 
   const filteredUsers = users.filter((user) =>
@@ -27,19 +44,39 @@ const Classes = () => {
 
   const userCard = (name, age, email) => (
     <>
-      <div className="col-span-1 flex flex-row gap-3 rounded-lg bg-slate-800 p-4">
+      <div className="col-span-1 flex flex-row gap-4 rounded-lg bg-slate-800 p-4">
         <img
           src="https://picsum.photos/200/300"
           alt="personal avatar image"
-          className="h-12 w-12 rounded-full"
+          className="h-12 w-12 min-w-max rounded-full"
         />
-
-        <div className="flex w-full flex-col">
-          <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-1 flex-col">
+          <div className="flex flex-row items-center justify-between pb-2">
             <h2 className="text-xl font-bold">{name}</h2>
-            <div className="flex flex-row gap-2">
-              <BsPencil className="text-lg text-slate-300" />
-              <RxCross1 className="text-lg text-slate-300" />
+            <div className="flex flex-row items-center gap-2">
+              <RxPencil1 className="text-lg text-slate-500" />
+              <RxCross1
+                className="cursor-pointer text-lg text-red-500"
+                onClick={() => removeUser(name)}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row gap-4 font-medium">
+            <div
+              className="flex cursor-pointer flex-row items-center gap-1 text-slate-300"
+              title="User's Age"
+              onClick={() => copyToClipboard(age)}
+            >
+              <BiCake className=" text-lg text-pink-500" />
+              <p>{age}</p>
+            </div>
+            <div
+              title="User's Email"
+              className="flex cursor-pointer flex-row items-center gap-1 text-slate-300"
+              onClick={() => copyToClipboard(email)}
+            >
+              <AiOutlineMail className=" text-lg text-blue-500" />
+              <p>{email}</p>
             </div>
           </div>
         </div>
