182e80569f857d7211b510924e7537543a2ba770
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-08 17:35:13 +0300

    modified index.js in src

--- src/index.js
+++ src/index.js
@@ -1430,3 +1430,13 @@ cannot override these now
   thermos.temperature = 26;
   temp = thermos.temperature;
 }
+{
+  const uppercaseString = (string) => {
+    return string.toUpperCase();
+  };
+  const lowercaseString = (string) => {
+    return string.toLowerCase();
+  };
+
+  export { uppercaseString, lowercaseString };
+}
