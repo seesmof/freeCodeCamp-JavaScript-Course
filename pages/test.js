ba85bcb5273021b966a6f02aad9ff37face0718f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-12 09:52:16 +0300

    modified test.js in pages

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,19 @@
-const car = {
-  make: "Honda",
-  model: "Accord",
-  year: 2020,
-};
-console.log(car);
+class Car {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start() {
+    console.log(`${this.make} ${this.model} started`);
+    return `${this.make} ${this.model} started`;
+  }
+}
+
+const myFirstCar = new Car("Honda", "Mondeo", 1998);
+console.log(myFirstCar);
+myFirstCar.start();
+
+const mySecondCar = new Car("Toyota", "Corolla", 2001);
+mySecondCar.start();
