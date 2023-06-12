8809e8e79c685fb3d49e89a97d390471f22d176b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-12 07:05:52 +0000

    🌿: Monday, June 12, 2023 at 10:05:52 AM Eastern European Summer Time

--- pages/test.js
+++ pages/test.js
@@ -1,19 +1,7 @@
-class Car {
-  constructor(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-  }
-
-  start() {
-    console.log(`${this.make} ${this.model} started`);
-    return `${this.make} ${this.model} started`;
-  }
+try {
+  let result = 1 / 0;
+  console.log(result);
+} catch (e) {
+  console.log("ERROR OCCURED");
+  console.log(e.message);
 }
-
-const myFirstCar = new Car("Honda", "Mondeo", 1998);
-console.log(myFirstCar);
-myFirstCar.start();
-
-const mySecondCar = new Car("Toyota", "Corolla", 2001);
-mySecondCar.start();
