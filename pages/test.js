f1b5f9c539c830b16e74a2e15d70e869ca0a0c26
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-15 17:07:32 +0300

    modified test.js in pages

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,50 @@
-const getNumber = (number) => {
-  return new Promise((resolve, reject) => {
-    if (typeof number === "number") {
-      resolve(number);
-    }
-    reject("ERROR: The value is not a number");
-  });
+const retrieveJoke = async () => {
+  const response = await fetch("https://api.chucknorris.io/jokes/random");
+  const data = await response.json();
+  return data;
 };
 
-console.log(getNumber(5));
-console.log(getNumber("Hi"));
+const postJoke = async (body, title) => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      body: body,
+    }),
+  });
+  const data = await response.json();
+  return data;
+};
+
+const putJoke = async (title) => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+    }),
+  });
+  const data = await response.json();
+  return data;
+};
+
+const main = async () => {
+  const joke = await retrieveJoke();
+  console.log(joke.value);
+
+  const title = "Test";
+  const body = "Test";
+  const post = await postJoke(body, title);
+  console.log(post);
+
+  const newTitle = "New Test";
+  const put = await putJoke(newTitle);
+  console.log(put);
+};
+
+main();
