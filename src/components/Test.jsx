34619d42bd1a684ae8df970cf69c8ecf5f11da17
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-11 10:02:33 +0000

    🌿: Tuesday, July 11, 2023 at 1:02:33 PM Eastern European Summer Time

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,13 +1,28 @@
-const Test = () => {
-  const count = 1;
+import { sculptureList } from "./data.js";
+import React from "react";
 
+export default function Gallery() {
+  const [index, setIndex] = React.useState(0);
+
+  let sculpture = sculptureList[index];
   return (
-    <ul>
-      <li>Item {count}</li>
-      <li>Item {count + 1}</li>
-      <li>Item {count + 2}</li>
-    </ul>
+    <>
+      <button
+        onClick={() => {
+          setIndex(index === sculptureList.length - 1 ? 0 : index + 1);
+        }}
+      >
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
   );
-};
-
-export default Test;
+}
