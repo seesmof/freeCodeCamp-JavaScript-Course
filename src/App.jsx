a96ee9bea13fc8f0e5486b545d8d33109e032e59
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-14 10:50:59 +0300

    changed 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import MultidimensionalArrays from "./components/MultidimensionalArrays";
 import StringVariables from "./components/StringVariables";
 import StructureHomework from "./components/StructureHomework";
 import Variables from "./components/Variables";
-import Test from "./components/Test";
 
 function App() {
   return (
@@ -17,8 +16,6 @@ function App() {
       <div class="mx-auto max-w-6xl p-4 sm:p-6">
         <Hero />
         <main class="flex flex-col gap-4 py-4 sm:gap-6 sm:py-6">
-          <Test />
-
           {/* <JsonPlaceholderFetchTesting /> */}
 
           <MultidimensionalArrays />
