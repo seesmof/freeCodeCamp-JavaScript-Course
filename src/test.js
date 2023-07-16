b280a6dd7a30d0d1b61fe38ee740216bdc10b2be
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-16 13:40:01 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,6 +1,10 @@
-const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
-let arr2;
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [, , ...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
 
-arr2 = [...arr1]; // Change this line
-
-console.log(arr2);
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+console.log(sourceWithoutFirstTwo);
