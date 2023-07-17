7abbdc83a8ae1363eb317ba29bcbf071a411792a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-17 21:47:07 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -4,6 +4,13 @@ const person = {
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
+  set fullName(value) {
+    [this.firstName, this.lastName] = value.split(" ");
+  },
 };
 
 console.log(person.fullName);
+person.fullName = "Jane Doe";
+console.log(
+  `${person.firstName} is ${person.lastName} and she is a ${person.fullName}`
+);
