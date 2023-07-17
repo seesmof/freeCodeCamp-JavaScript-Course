569fbc485f64ac2b1732c35ef2cbabfc5e38b58d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-17 21:38:17 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,19 +1,10 @@
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"],
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
 };
-function makeList(arr) {
-  // Only change code below this line
-  const failureItems = [];
-  arr.forEach((item) => {
-    if (result.failure.includes(item)) {
-      failureItems.push(`<li class="text-warning">${item}</li>`);
-    }
-  });
-  // Only change code above this line
-
-  return failureItems;
-}
-
-const failuresList = makeList(result.failure);
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
