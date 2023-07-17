feeda919693a8b53c90c655468597cc0c8026bbb
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-17 21:56:57 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,20 +1,7 @@
-// Only change code below this line
-class Thermostat {
-  constructor(temp) {
-    this.temp = temp;
-  }
-
-  get temperature() {
-    return (5 / 9) * (this.temp - 32);
-  }
-
-  set temperature(newTemp) {
-    this.temp = (9 / 5) * newTemp + 32;
-  }
-}
-// Only change code above this line
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
+<html>
+  <body>
+    <!-- Only change code below this line -->
+    <script type="module" src="index.js"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
