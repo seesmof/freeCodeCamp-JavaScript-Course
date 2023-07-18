fa3b1f8bfa1a3a61035236334c0e74306ffae8e7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 08:37:29 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,5 +1,3 @@
-const str = "The quick brown fox jumps over the lazy dog";
-const pattern = /fox/;
-
-const matches = str.match(pattern);
-console.log(matches);
+let testStr = "Hello, my name is Kevin.";
+let testRegex = /Kevin/;
+console.log(testRegex.test(testStr));
