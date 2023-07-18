1029213d6fff32860cc03547c328879628623526
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 12:30:21 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/gi; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
-console.log(result); // Change this line
+const humStr = "I'll hum a song";
+const hugStr = "Bear hug";
+const pattern = /hu./gi;
+console.log(humStr.match(pattern));
+console.log(hugStr.match(pattern));
