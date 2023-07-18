be85e226c70bab7c3c73a944867f2ca39e3f31ed
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 08:43:42 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,3 +1,13 @@
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
-let waldoRegex = /Waldo/; // Change this line
-let result = waldoRegex.test(waldoIsHiding);
+const str = "The quick brown fox jumps over the lazy dog";
+const stringsToFind = ["fox", "dog"];
+const matches = [];
+
+for (const searchString of stringsToFind) {
+  let index = str.indexOf(searchString);
+  while (index !== -1) {
+    matches.push({ searchString, index });
+    index = str.indexOf(searchString, index + 1);
+  }
+}
+
+console.log(matches);
