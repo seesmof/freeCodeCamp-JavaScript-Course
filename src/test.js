d101e23e70240945635aceb993d5d84269e8dec9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 08:44:41 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,13 +1,3 @@
-const str = "The quick brown fox jumps over the lazy dog";
-const stringsToFind = ["fox", "dog"];
-const matches = [];
-
-for (const searchString of stringsToFind) {
-  let index = str.indexOf(searchString);
-  while (index !== -1) {
-    matches.push({ searchString, index });
-    index = str.indexOf(searchString, index + 1);
-  }
-}
-
-console.log(matches);
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
