76329e3d2a84e8a8f8c4c063a7a2f88c24006156
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 12:45:15 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-let quoteSample = "3 blind mice.";
-let myRegex = /[^1-9aeiou]/gi; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
-console.log(result); // Change this line
+const testingString =
+  "The quick brown fox jumps over the lazy dog. This is a sentence that contains numbers like 123 and 456. It also has some special characters like !@#$%^&*(). This is a medium-sized text that you can use to test your regular expression in JavaScript. Hopefully, it will help you ensure that your regular expression works as expected.";
+const regex = /[a-c]+/gi;
+console.log(testingString.match(regex));
