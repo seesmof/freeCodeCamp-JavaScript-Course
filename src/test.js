36ebde7ed6676123247600df3f44a3d5dfc57de5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 12:58:25 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,7 +1,8 @@
-const soccer = "goooooooooal!";
-const g = "gut feeling";
-const o = "over the moon";
-const go = /go*/gi;
+const chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
 
-console.log(`${soccer.match(go)} ${g.match(go)} ${o.match(go)}`);
-console.log(g.match(go));
+// Only change code below this line
+let chewieRegex = /Aa*/g; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+console.log(result);
