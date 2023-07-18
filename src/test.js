df000c70a2483d8e3e06cf8b4568f782a656a52f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 09:03:40 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-const string =
-  'Regular expressions, abbreviated as regex, or sometimes regexp, are one of those concepts that you probably know is really powerful and useful. But they can be daunting, especially for beginning programmers. JavaScript includes several helpful methods that make using regular expressions much more manageable. Of the included methods, the .match(), .matchAll(), and .replace() methods are probably the ones you\'ll use most often. But say you want to see how many times the word "are" occurs in a string. To do that, just add the global search flag to your regular expression.';
-const pattern = /dog/gi;
-console.log(string.match(pattern));
+console.log("Hello, World!".match(/Hello/g));
+const ourStr = "Regular expressions";
+const ourRegex = /expressions/gi;
+console.log(ourStr.match(ourRegex));
