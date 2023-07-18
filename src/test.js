2808e70b088ae8bf93f0eabbee6450391e52cc33
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 08:38:08 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,3 +1,3 @@
-let testStr = "Hello, my name is Kevin.";
-let testRegex = /Kevin/;
-console.log(testRegex.test(testStr));
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
