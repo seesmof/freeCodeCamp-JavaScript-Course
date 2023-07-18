df20df2383cebb1067a32b28713cb707cd768c04
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 12:49:26 +0300

    modified test.js added regex_testing.js

---
+++ src/regex_testing.js
@@ -0,0 +1,4 @@
+const testingString =
+  "The quick brown fox jumps over the lazy dog. This is a sentence that contains numbers like 123 and 456. It also has some special characters like !@#$%^&*(). This is a medium-sized text that you can use to test your regular expression in JavaScript. Hopefully, it will help you ensure that your regular expression works as expected.";
+const regex = /[a-c]*/gi;
+console.log(testingString.match(regex));
