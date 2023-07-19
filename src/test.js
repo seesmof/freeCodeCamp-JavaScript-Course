9646156b8bc642a5c9a19b12f72641496f079613
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:42:37 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,3 +1,3 @@
-const password = "abc123";
-const regex = /(?=\w{3,6})(?=\D*\d)/;
-console.log(regex.test(password));
+const test = "Pumpkin";
+const regex = /P(engu|umpk)in/g;
+console.log(test.match(regex));
