dc919a579b40c6ce86644810c1d09829c6e27b9f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 15:32:42 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,15 +1,5 @@
-const long = /[A-Za-z0-9_]+/;
-const short = /\w+/;
-
-const numbers = ["42", "31", "54", "98"];
-const names = ["important_var", "this_is_cool", "remember_that"];
-
-numbers.forEach((number) => {
-  console.log(number.match(short));
-  console.log(number.match(long));
-});
-console.log(`\n`);
-names.forEach((name) => {
-  console.log(name.match(short));
-  console.log(name.match(long));
-});
+const short = /\W/g;
+const number = "42%";
+const sentence = "Coding!..";
+console.log(sentence.match(short));
+console.log(number.match(short));
