74f8622955dc365605ba7413dfd7f3e892943d8e
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:32:08 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,9 +1,7 @@
 const A4 = "haaaah";
-const A2 = "haah";
-const A100 = `h${"a".repeat(100)}h`;
-console.log(A100);
-
-const regex = /ha{3,}h/gi;
-console.log(A4.match(regex));
-console.log(A2.match(regex));
+const A3 = "haaah";
+const A100 = `a${"a".repeat(100)}h`;
+const regex = /ha{3}h/gi;
 console.log(A100.match(regex));
+console.log(A3.match(regex));
+console.log(A4.match(regex));
