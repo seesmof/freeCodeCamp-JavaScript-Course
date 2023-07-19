618931fe5d5ee5d90acd4462879f546c95a4ecd7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:36:25 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -2,5 +2,5 @@ const quit = "qu";
 const noquit = "qt";
 const quRegex = /q(?=u)/gi;
 const qRegex = /q(?!u)/gi;
-console.log(quRegex.test(quit));
-console.log(qRegex.test(quit));
+console.log(quit.match(quRegex));
+console.log(noquit.match(qRegex));
