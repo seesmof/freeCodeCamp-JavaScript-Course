f8cefce0c170ea95dd0406dac764083352d7eb22
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:30:42 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,5 +1,9 @@
-const A4 = "aaaah";
-const A2 = "aah";
-const regex = /a{3,5}h/gi;
+const A4 = "haaaah";
+const A2 = "haah";
+const A100 = `h${"a".repeat(100)}h`;
+console.log(A100);
+
+const regex = /ha{3,}h/gi;
 console.log(A4.match(regex));
 console.log(A2.match(regex));
+console.log(A100.match(regex));
