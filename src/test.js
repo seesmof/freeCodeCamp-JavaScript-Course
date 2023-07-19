19e6e4a774af577bf490c5cc04abaa541356f2c5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:28:11 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,3 +1,3 @@
 const whiteSpace = "Whitespace. Whitespace everywhere!";
-const spaceRegex = /\s/g;
-console.log(whiteSpace.match(spaceRegex));
+const spaceRegex = /\S/g;
+console.log(whiteSpace.match(spaceRegex).length);
