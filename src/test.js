80a4c5c01cf9d539b00ce65947281a488e4a2ea5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 20:28:44 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,3 +1,5 @@
-const whiteSpace = "Whitespace. Whitespace everywhere!";
-const spaceRegex = /\S/g;
-console.log(whiteSpace.match(spaceRegex).length);
+const A4 = "aaaah";
+const A2 = "aah";
+const regex = /a{3,5}h/gi;
+console.log(A4.match(regex));
+console.log(A2.match(regex));
