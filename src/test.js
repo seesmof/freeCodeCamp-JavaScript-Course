aae3329840b848b3c7855bfd4c4fed04ac008e97
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 15:24:19 +0300

    modified test.js in src

--- src/test.js
+++ src/test.js
@@ -1,2 +1,6 @@
-const findCriminals = /C+/g;
-console.log(findCriminals.test("P1P5P4CCCcP2P6P3"));
+const first = "Ricky is first and can be found.";
+const regex = /^Ricky/;
+console.log(first.match(regex));
+
+const second = "You can't find Ricky now";
+console.log(regex.test(second));
