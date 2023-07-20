6dfc097e8ea44dd65d87aad0cd036a2058b63364
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-20 19:01:52 +0300

    modified test.js in JS

--- src/JS/test.js
+++ src/JS/test.js
@@ -1,4 +1,4 @@
-const repeatRegex = /(\w+) \1 \1/gim;
+const repeatRegex = /^(\d+) \1 \1$/gim;
 const repeatStr = "row row row row your boat";
 const matches = repeatRegex.exec(repeatStr);
 console.log(matches);
