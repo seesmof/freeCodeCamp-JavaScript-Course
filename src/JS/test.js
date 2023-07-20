0d8cb341f0d0fb0791f03880705111a33397ba1d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-20 18:56:46 +0300

    modified test.js in JS

--- src/JS/test.js
+++ src/JS/test.js
@@ -1,4 +1,4 @@
-const repeatStr = "row row row your boat";
-const regex = /(\w+)\s\1+/gm;
+const repeatStr = "   row row row your boat   ";
+const regex = /^\s[\W\D]*\s$/gm;
 const matches = repeatStr.match(regex);
 console.log(matches);
