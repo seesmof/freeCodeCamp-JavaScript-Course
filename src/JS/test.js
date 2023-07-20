29ff0112f30ec84c75f45ae4642960b5f2fe8a9a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-20 18:57:36 +0300

    modified test.js in JS

--- src/JS/test.js
+++ src/JS/test.js
@@ -1,4 +1,4 @@
-const repeatStr = "   row row row your boat   ";
-const regex = /^\s[\W\D]*\s$/gm;
-const matches = repeatStr.match(regex);
+const repeatRegex = /(\w+) \1 \1/gim;
+const repeatStr = "row row row row your boat";
+const matches = repeatRegex.exec(repeatStr);
 console.log(matches);
