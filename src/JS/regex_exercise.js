94bd12cbd50b36f7cee54612b6f73eb8d24fd5a1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-20 10:18:25 +0300

    modified regex_exercise.js in JS

--- src/JS/regex_exercise.js
+++ src/JS/regex_exercise.js
@@ -1,28 +1,37 @@
 // Write a regex to match a string that starts with 'hello' and ends with 'world'.
 const oneText = "hello world";
-const oneRegex = /^hello.*world$/;
-const oneOutput = `1. ${oneText.match(oneRegex)}`;
+const oneRegex = /^hello.*world$/g;
+const oneOutput = `1. ${oneText.match(oneRegex)}\n`;
 
 // Write a regex to match a string that contains a number.
 const twoText = "This text contains numbers such as 321, 945 and 1643.";
 const twoRegex = /\d/g;
-const twoOutput = `2. ${twoText.match(twoRegex)}`;
+const twoOutput = `2. ${twoText.match(twoRegex)}\n`;
 
 // Write a regex to match a valid email address. Hint: use quantifiers and character classes.
-const threeText = [
-  "zo@gafikkar.qa",
-  "kuf@kuovageg.om",
-  "ihci@tawsuwhuj.ee",
-  "ojetuj@dif.ao",
-];
-const threeRegex = /^(\w+){2,}@(\w+){2,}\.(\w+){2,}$/;
-const threeOutput = `3. ${threeText[0].match(threeRegex)}`;
+const threeText = `zo@gafikkar.qa
+  kuf@kuovageg.om
+  ihci@tawsuwhuj.ee
+  ojetuj@dif.ao`;
+const threeRegex = /^(\w+){2,}@(\w+){2,}\.(\w+){2,}$/gm;
+const threeOutput = `3. ${threeText.match(threeRegex)}\n`;
 
 // Write a regex to match a string that contains only uppercase or lowercase letters.
+const fourText =
+  "pride division two queen summer struck night student immediately organized held keep model everybody musical number climate split military mainly visitor depth atomic basic";
+const fourRegex = /[a-zA-Z]/g;
+const fourOutput = `4. ${fourText.match(fourRegex)}\n`;
 
 // Write a regex to match a valid URL. Hint: use quantifiers, character classes and grouping.
+const fiveText = "http://wulur.ma/nuksut";
+const fiveRegex = /https?:\/\/(www\.)?\w+\.\w+/g;
+const fiveOutput = `5. ${fiveText.match(fiveRegex)}\n`;
 
 // Write a regex to match a string that contains any word repeated twice, like 'hello hello'.
+const sixText =
+  "hello hello hello giving wooden form held count author stock relationship advice lie dry cut deer sport out swing excitement atomic topic fuel nearly doing getting path";
+const sixRegex = /wut/g;
+const sixOutput = `6. ${sixText.match(sixRegex)}\n`;
 
 // Write a regex to match a valid hexadecimal color code like #FFF, #123456.
 
