66b4751a96674dfcb6c61bbcf8e5b60cf7c8755c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-27 16:15:57 +0300

    update test.js

--- src/JS/test.js
+++ src/JS/test.js
@@ -1,4 +1,40 @@
-let hello = "   Hello, World!  ";
-let wsRegex = /^\s+|\s+$/g; // Change this line
-let result = hello.replace(wsRegex, ""); // Change this line
-console.log(result);
+const add = (a, b) => {
+  return a + b;
+};
+
+const multiplyByTwo = (x) => x * 2;
+
+const greet = (person = `Hello, ${person}`);
+
+const getFullName = (firstName, lastName, middleName = "") =>
+  `${lastName} ${firstName} ${middleName}`;
+
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const {
+  firstName,
+  lastName,
+  address: { city },
+} = user;
+const userInfo = { firstName, lastName, city, isAdult: true };
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.getArea());
