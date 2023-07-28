5474102163188875f6f87176dc7914b47e4cb8c9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-28 09:19:36 +0300

    update test.js

--- src/JS/test.js
+++ src/JS/test.js
@@ -1,40 +1,4 @@
-const add = (a, b) => {
-  return a + b;
-};
-
-const multiplyByTwo = (x) => x * 2;
-
-const greet = (person = `Hello, ${person}`);
-
-const getFullName = (firstName, lastName, middleName = "") =>
-  `${lastName} ${firstName} ${middleName}`;
-
-const user = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 30,
-  address: {
-    city: "New York",
-    country: "USA",
-  },
-};
-
-const {
-  firstName,
-  lastName,
-  address: { city },
-} = user;
-const userInfo = { firstName, lastName, city, isAdult: true };
-
-class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  getArea() {
-    return this.width * this.height;
-  }
-}
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.getArea());
+let output =
+  "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
